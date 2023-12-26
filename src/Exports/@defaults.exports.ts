c2a3b20f2f038f603523067bf953d2996dff6a1b
janekeilts <janekeilts.janekeilts.com>
2023-12-26 13:06:21 +0000

    Initial commit

---
+++ src/Exports/@defaults.exports.ts
@@ -0,0 +1,10 @@
+/*
+import { routes } from 'src/app/app.routes';
+import { appConfig } from 'src/app/app.config';
+import { AppComponent } from 'src/app/app.component';
+export {
+  routes,
+  appConfig,
+  AppComponent,
+};
+*/
